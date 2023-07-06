fde76fa1c308d23a5701a83605657bb3420018c0
minababaee <minaababaee@gmail.com>
2023-07-06 11:45:53 -0700

    first commit

---
+++ textconvertor.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import *
+
+main = Tk()
+main.title("text convertor")
+main.minsize(400,400)
+main.maxsize(400,400)
+
+def widgets():
+    pass
+
+widgets()
+
+main.mainloop()
